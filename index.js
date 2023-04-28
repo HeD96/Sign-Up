6b5abb9660f50022e0bc94c4997a1cc170dba717
imhed <hedplanet96@gmail.com>
2023-04-29 00:04:39 +0300

    added password matching

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-document.getElementsByClassName("name");
+let password = document.getElementById("password");
+let password_confirm = document.getElementById("confirm");
+let firstName = document.getElementById("first_name");
+let lastName = document.getElementById("last_name");
+let phoneNum = document.getElementById("phone_num");
+
+phoneNum.value = "+7 ";
+
+password.addEventListener("keyup", function() {
+    if (password_confirm.value !== "") {
+        CheckPassword();
+    }
+});
+
+password_confirm.addEventListener("keyup", CheckPassword);
+
+function CheckPassword() {
+    if (password.value === password_confirm.value) {
+        alert("Yay!");
+    }
+}
+
+
+
+
