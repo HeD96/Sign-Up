58bdf3ff72f7732dbe79300347e0cf4705c0e5f3
imhed <hedplanet96@gmail.com>
2023-04-29 00:24:18 +0300

    visual confirmation of password added, deleted form labels' events

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ let password_confirm = document.getElementById("confirm");
 let firstName = document.getElementById("first_name");
 let lastName = document.getElementById("last_name");
 let phoneNum = document.getElementById("phone_num");
+let pass = document.querySelectorAll(".pass");
 
 phoneNum.value = "+7 ";
+
+//password matching
 
 password.addEventListener("keyup", function() {
     if (password_confirm.value !== "") {
@@ -16,7 +19,15 @@ password_confirm.addEventListener("keyup", CheckPassword);
 
 function CheckPassword() {
     if (password.value === password_confirm.value) {
-        alert("Yay!");
+        pass.forEach(function(outline) {
+            outline.classList.remove("error");
+            outline.classList.add("valid");
+        });
+    } else {
+        pass.forEach(function(outline) {
+            outline.classList.remove("valid");
+            outline.classList.add("error");
+        });
     }
 }
 
