c5eb1eead23632774e9da0532e98b077b694df38
imhed <hedplanet96@gmail.com>
2023-04-29 20:05:34 +0300

    added phone number country code

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+//password fields
 let password = document.getElementById("password");
 let password_confirm = document.getElementById("confirm");
+let pass = document.querySelectorAll(".pass");
+let errorMessage = document.getElementById("error_message");
+//name fields
 let firstName = document.getElementById("first_name");
 let lastName = document.getElementById("last_name");
+//email field
+let email = document.getElementById("email");
+//phone number field
 let phoneNum = document.getElementById("phone_num");
-let pass = document.querySelectorAll(".pass");
 
-phoneNum.value = "+7 ";
+
+//phone number formatting
+
+phoneNum.addEventListener("focus", function() {
+    if (phoneNum.value === "") {
+        phoneNum.value = "+7 ";
+    }
+});
+
 
 //password matching
 
@@ -18,15 +32,18 @@ password.addEventListener("keyup", function() {
 password_confirm.addEventListener("keyup", CheckPassword);
 
 function CheckPassword() {
+
     if (password.value === password_confirm.value) {
         pass.forEach(function(outline) {
             outline.classList.remove("error");
             outline.classList.add("valid");
+            errorMessage.classList.add("invisible");
         });
     } else {
         pass.forEach(function(outline) {
             outline.classList.remove("valid");
             outline.classList.add("error");
+            errorMessage.classList.remove("invisible");
         });
     }
 }
